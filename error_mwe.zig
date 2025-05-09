0de16b9f219f5c9b35e1ebea8f498f7f38aa96fc
Eric Tian <erictianc@outlook.com>
2025-05-09 22:38:06 +0800

    error_mwe.zig 新版

---
+++ error_mwe.zig
@@ -0,0 +1,21 @@
+//! 速通 openFile Error
+
+const std = @import("std");
+
+pub fn read(filepath: []const u8) !void {
+    var home = std.fs.cwd();
+    defer home.close();
+
+    var file = try home.openFile(filepath, .{});
+    defer file.close();
+}
+
+pub fn main() !void {
+    std.debug.print("已在当前目录下创建 test 文件\n", .{});
+    var file = try std.fs.cwd().createFile("test", .{});
+    file.close();
+
+    try read("test");
+    // try read("test");
+    std.debug.print("现在打开 zig 源代码文件为上面这行取消注释，再试一试", .{});
+}
